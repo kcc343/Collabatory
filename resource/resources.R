403b66a1614771718de5da01f7479de396124a22
Emily Zhang <emily.aiwen@gmail.com>
2018-05-14 20:19:11 -0700

    Added new folder and new resources

---
+++ resource/resources.R
@@ -0,0 +1,22 @@
+# Here are some important and useful resources to create a visualization
+# for the project
+
+# Data Visualization Examples
+https://informationisbeautiful.net/
+
+# Tools
+  # Scrolling Tools
+  https://github.com/1wheel/graph-scroll
+  http://1wheel.github.io/graph-scroll/
+    
+  https://github.com/Prinzhorn/skrollr/tree/master/examples
+  
+  # Visualization Tools
+  https://thenextweb.com/dd/2015/04/21/the-14-best-data-visualization-tools/
+  https://econsultancy.com/blog/66131-17-visualisation-tools-to-make-your-data-beautiful
+  
+    
+  # Charts
+  https://developers.google.com/chart/
+  http://www.chartjs.org/samples/latest/ # https://github.com/chartjs/Chart.js
+  
