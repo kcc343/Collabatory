8f905cfc80624b0908cacd66f0b5e0dcd89eb765
Kelly Chhor <kcc343@gmail.com>
2018-05-28 11:09:43 -0700

    write csv file

--- scripts/popular_list.R
+++ scripts/popular_list.R
@@ -8,7 +8,7 @@ library(jsonlite)
 
 source("api_key.R")
 
-# Create HTTP request to get get popular movies (first 20)
+# Create HTTP request to get popular movies (first 20)
 
 base_uri <- "https://api.themoviedb.org/3"
 resource <- "/movie/popular"
@@ -25,35 +25,32 @@ popular_list <- fromJSON(body)
 
 popular_data <- list()
 
-query_params <- list(api_key = api_key, append_to_response = "credits")
-
+count <- 0
 for (num in 1:length(popular_list$results$id)) {
   resource <- paste0("/movie/", popular_list$results$id[num])
   uri_full <- paste0(base_uri, resource)
   response <- GET(uri_full, query = query_params)
   body <- content(response, "text")
-  popular_movie <- fromJSON(body)
-  popular_data[[num]] <- popular_movie
+  popular_data[[num]] <- fromJSON(body)
+  count <- count +  1
+  if (count > 3) {
+    count = 0
+    Sys.sleep(10)
+  }
 }
 
 budget <- c()
-genres <- list()
-homepage <- c()
 status <- c()
-original_title <- c()
 overview <- c()
 popularity <- c()
-production_companies <- list()
-production_countries <- c()
 release_date <- c()
 revenue <- c()
 runtime <- c()
 title <- c()
-credits <- c()
+
 
 for (num in 1:length(popular_data)) {
   budget <- c(budget, popular_data[[num]]$budget)
-  original_title <- c(original_title, popular_data[[num]]$original_title)
   overview <- c(overview, popular_data[[num]]$overview)
   popularity <- c(popularity, popular_data[[num]]$popularity)
   status <- c(status, popular_data[[num]]$status)
@@ -61,7 +58,10 @@ for (num in 1:length(popular_data)) {
   revenue <- c(revenue, popular_data[[num]]$revenue)
   runtime <- c(runtime, popular_data[[num]]$runtime)
   title <- c(title, popular_data[[num]]$title)
-  credits <- c(credits, popular_data[[num]]$credits[1])
 }
 
-get_details <- data.frame(budget, revenue, title, overview, popularity, release_date, status)
+get_details <- data.frame(budget, revenue, title, overview, popularity, release_date, status, stringsAsFactors = F)
+
+write.csv(get_details, file = "../files/popular_movies.csv")
+
+
