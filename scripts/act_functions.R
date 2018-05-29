67f1419b2a91c07fc86121571b94bcac18eda2bc
Kelly Chhor <kcc343@gmail.com>
2018-05-29 16:20:42 -0700

    change to ggplotly for plotting

--- scripts/act_functions.R
+++ scripts/act_functions.R
@@ -1,26 +1,42 @@
 library(tools)
+library(dplyr)
+library(plotly)
+library(ggplot2)
+
 act_analysis <- function(name, type) {
   data <- read.csv(
-    paste0("../files/", tolower(name), "_df.csv"),
+    paste0("./files/", tolower(name), "_df.csv"),
     stringsAsFactors = FALSE
   )
 
   data <- data %>%
     filter(budget != 0 & revenue != 0)
 
-  p <- plot_ly(data,
-    y = data[, type],
-    name = toTitleCase(name),
-    type = "box",
-    boxpoints = "all",
-    hoverinfo = "text",
-    text = paste0(
-      "title: ", data[, "title"], "<br />",
-      type, ": ", data[, type]
+  p <- ggplot(data, aes(x = factor(1), y = data[, type], text = paste0(type , ": ", data[, type]))) +
+    geom_boxplot() + 
+    geom_jitter(size = 2, position = position_jitter(0.2)) +
+    ggtitle(paste0(toTitleCase(name), "'s movie ", type, " boxplot")) +
+    theme(
+      axis.text.x = element_blank()
     )
-  ) %>%
-    layout(
-      title = paste0(toTitleCase(name), "'s movie ", type, " boxplot")
-    )
-  p
+  
+  ggplotly(p, tooltip = c("text"))
+  
+  # p <- plot_ly(data,
+  #   y = data[, type],
+  #   name = toTitleCase(name),
+  #   type = "box",
+  #   boxpoints = "all",
+  #   hoverinfo = "text",
+  #   text = paste0(
+  #      "title: ", data[, "title"], "<br />",
+  #      type, ": ", data[, type]
+  #   )
+  # ) %>%
+  #   layout(
+  #     title = paste0(toTitleCase(name), "'s movie ", type, " boxplot")
+  #   )
+  # p
 }
+
+act_analysis("Matt Damon", "revenue")
