11180d146104ad641d13585c9434ee5c9e441f72
Kelly Chhor <kcc343@gmail.com>
2018-05-29 23:27:00 -0700

    add comments and add filter data code

--- scripts/act_functions.R
+++ scripts/act_functions.R
@@ -1,15 +1,26 @@
+# Load libraries
+
 library(tools)
 library(dplyr)
 library(plotly)
 library(ggplot2)
 library(scales)
 
+# Access each of the actor/actress csv files and returns a box plot for specified data
+# (budget or revenue)
 act_analysis <- function(name, type) {
+  
+  # Read in csv data of actor/actress
   data <- read.csv(
     paste0("./files/", tolower(name), "_df.csv"),
     stringsAsFactors = FALSE
   )
-
+  
+  # filter data so there is no zero budget or revenue
+  data <- data %>%
+    filter(budget != 0 & revenue != 0)
+  
+  # Plot data into box plot
   p <- ggplot(data, 
               aes(x = factor(1),
                   y = data[, type], 
