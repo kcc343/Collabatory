9370612a912ea933a9a4d076b84c6d5a845e125a
Kelly Chhor <kcc343@gmail.com>
2018-05-30 00:10:16 -0700

    add comments and fix lintr errors

--- scripts/company_movies.R
+++ scripts/company_movies.R
@@ -18,17 +18,22 @@ companies_list <- c(
   "Warner Bros. Pictures",
   "20th Century Fox"
 )
+
 # Get movie companies IDs
 
 base_uri <- "https://api.themoviedb.org/3"
 ids <- c()
+
 for (i in 1:length(companies_list)) {
+
+  # Create HTTP request to search for company
+
   resource <- "/search/company"
   uri_full <- paste0(base_uri, resource)
   query_params <- list(api_key = api_key, query = companies_list[i])
-  
-  # Send HTTP request
-  
+
+  # Send HTTP request to search for company and get ID
+
   response <- GET(uri_full, query = query_params)
   body <- content(response, "text")
   company_info <- fromJSON(body)
@@ -36,25 +41,36 @@ for (i in 1:length(companies_list)) {
   ids[i] <- company_id
 }
 
+# Get "20th Century Fox ID
+
 ids[length(companies_list)] <- 25
 
 for (id in 1:length(ids)) {
   get_details <- list()
-  
+
+  # Create HTTP request to get movie made by movie company
+
   resource <- "/discover/movie"
   uri_full <- paste0(base_uri, resource)
   query_params <- list(
     api_key = api_key,
     with_companies = ids[id]
   )
-  
+
+  # Send HTTP request
+
   response <- GET(uri_full, query = query_params)
   body <- content(response, "text")
   movie_data <- fromJSON(body)
-  
+
+  # Assign first page of results to a data frame
+
   df_string <- paste0(companies_list[id], "_movies")
   df <- assign(df_string, movie_data$results)
-  
+
+  # Get movie data from each page and let code wait 10 seconds for API requests
+  # to reset to 40
+
   count <- 0
   for (num in 2:movie_data$total_pages) {
     response <- GET(uri_full, query = c(query_params, page = num))
@@ -67,13 +83,20 @@ for (id in 1:length(ids)) {
       count <- 0
     }
   }
-  
+
+  # Based on movie IDs, get the data for the movies
+
   count <- 0
   for (num in 1:nrow(df)) {
+
+    # Create HTTP request to get movie data
+
     resource <- paste0("/movie/", df$id[num])
     uri_full <- paste0(base_uri, resource)
     query_params <- list(api_key = api_key)
-    
+
+    # Send HTTP request
+
     response <- GET(uri_full, query = query_params)
     body <- content(response, "text")
     one_page <- fromJSON(body)
@@ -84,7 +107,9 @@ for (id in 1:length(ids)) {
       count <- 0
     }
   }
-  
+
+  # Build lists for each data type
+
   budget <- list()
   status <- list()
   overview <- list()
@@ -93,7 +118,7 @@ for (id in 1:length(ids)) {
   release_date <- list()
   revenue <- list()
   title <- list()
-  
+
   for (num in 1:length(get_details)) {
     budget[num] <- get_details[[num]]$budget
     title[num] <- get_details[[num]]$title
@@ -104,8 +129,9 @@ for (id in 1:length(ids)) {
     release_date[num] <- get_details[[num]]$release_date
     revenue[num] <- get_details[[num]]$revenue
   }
-  
+
   # Convert empty values into NA
+
   budget[sapply(budget, is.null)] <- NA
   overview[sapply(overview, is.null)] <- NA
   popularity[sapply(popularity, is.null)] <- NA
@@ -114,25 +140,27 @@ for (id in 1:length(ids)) {
   release_date[sapply(release_date, is.null)] <- NA
   revenue[sapply(revenue, is.null)] <- NA
   title[sapply(title, is.null)] <- NA
-  
+
+  # Create data frame from lists of data
+
   assign_string <- paste0(companies_list[i], "_df")
   df <- assign(
     assign_string,
     data.frame(
-      budget = unlist(budget), 
-      revenue = unlist(revenue), 
-      title = unlist(title), 
-      overview = unlist(overview), 
+      budget = unlist(budget),
+      revenue = unlist(revenue),
+      title = unlist(title),
+      overview = unlist(overview),
       popularity = unlist(popularity),
       vote_count = unlist(vote_count),
-      release_date = unlist(release_date), 
+      release_date = unlist(release_date),
       status = unlist(status),
       stringsAsFactors = F
     )
   )
-  
+
+  # Create csv file from data frame and let code reset API requests to 40
+
   write.csv(df, file = paste0("../files/", companies_list[id], "_df.csv"))
   Sys.sleep(10)
 }
-
-# Get movies with company
